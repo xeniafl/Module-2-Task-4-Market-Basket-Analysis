2aa045cf3c69e372cc5d57d6bdca75710cfd5bcf
Xenia <xfabregatlluch@gmail.com>
2019-10-28 18:16:35 +0100

    trying to rename 1st column as product type

--- Market Basket Analysis.R
+++ Market Basket Analysis.R
@@ -1,46 +1,80 @@
-# hola hola
+# hola hola!
 
 # PLAN OF ATTACK STEP 2: INSTALL, UPLOAD AND GET TO KNOW YOUR DATA SET
 
 library(arules)
 library(arulesViz)
+library(readr)
+library(reshape2)
 
-electronidex_transactions <- 
+tr <- 
   read.transactions("C:/Users/user/Downloads/ElectronidexTransactions2017.csv", sep = ",")
 
+x <- as(tr, "matrix")
+
+a <- t(x)
+
+colnames(a)
+
+product_key <- read_delim("~/producttypekey.csv", 
+                             ";", escape_double = FALSE, trim_ws = TRUE)
+
+
+hola1 <- merge(a, product_key, by.x = "", by.y = "product name", all.x = TRUE)
+
+
+
+
+########
+
+
+
+
+
+
 inspect(electronidex_transactions)
+itemLabels(electronidex_transactions)
+LIST(electronidex_transactions)
 length (electronidex_transactions)
 size(electronidex_transactions)
-LIST(electronidex_transactions)
-itemLabels(electronidex_transactions)
+itemsbought <- sort(size(electronidex_transactions), decreasing = T)
+table(itemsbought)
+hist(itemsbought, breaks = 30)
+
+electronidex_transactions[order(size(electronidex_transactions))]
+sorted <- electronidex_transactions[order(size(electronidex_transactions))]
+B2B <- sorted[1:1000]
+B2C <- sorted[]
+
 
 # PLAN OF ATTACK STEP 3: VISUALIZE YOUR DATA SET
 
 itemFrequency(electronidex_transactions, type = "absolute")
 itemFrequencyPlot(electronidex_transactions, type = "absolute", topN = 10, horiz = F)
-image(electronidex_transactions[1:50])
+image(electronidex_transactions)
 
-sample(electronidex_transactions, 50)
-itemFrequencyPlot(sample(electronidex_transactions, 50))
-image(sample(electronidex_transactions, 50))
+set.seed(243)
+image(sample(electronidex_transactions, 100))
 
 # PLAN OF ATTACK STEP 4: APPLY THE APRIORI ALGORITHM
 
-RulesName<- apriori (electronidex_transactions, parameter = list(supp = 0.05, conf = 0.1))
+RulesName<- apriori (electronidex_transactions, parameter = list(supp = 0.01, conf = 0.5, minlen = 1))
 
 # PLAN OF ATTACK STEP 5: EVALUATE YOUR MODEL
 
+summary(RulesName)
 inspect(RulesName)
 
 # PLAN OF ATTACK STEP 6: IMPROVE YOUR MODEL
 
-inspect(sort(RulesName, by = "count"))
-ItemRules <- subset(RulesName, items %in% "iMac")
+inspect(sort(RulesName, by = "support"))
+
+# ItemRules <- subset(RulesName, items %in% "iMac")
 
 is.redundant(RulesName)
 
 # PLAN OF ATTACK STEP 7: VISUALIZE YOUR RESULTS
 
-plot(RulesName)
-plot(RulesName, method="graph", control=list(type="items")) 
+plot(RulesName, engine = "interactive")
+plot(RulesName, method="graph", control=list(type="items"), engine = "interactive") 
 
